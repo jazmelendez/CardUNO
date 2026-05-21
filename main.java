0e9415f9287e67169dc678926bab421bd9f91be3
jazmelendez <melendez29jaz@gmail.com>
2026-05-20 18:44:24 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,8 +1,24 @@
+/**
+ * Clase principal que sirve como punto de entrada para la aplicación CardUNO.
+ * En esta versión, se encarga de iniciar la interfaz gráfica (GameView).
+ * @author Alejandro Hernandez
+ * @author Abigail Martinez
+ * @author Jazmin Melendez
+ */
 public class Main {
-
+    /**
+     * Método principal que arranca la aplicación.
+     * Utiliza InvokeLater para asegurar que la interfaz se inicie correctamente 
+     * en el hilo de eventos de Java Swing.
+     * @param args Argumentos de la línea de comandos (no utilizados).
+     */
     public static void main(String[] args) {
-
-        Game game = new Game();
-        game.startGame();
+        // Esto le dice a Java que prepare la ventana de forma segura
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                // Creamos y mostramos la ventana que configuramos con los 5 paneles
+                new GameView();
+            }
+        });
     }
 }
